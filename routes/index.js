4af9ea6e7668d53d5a6d1a563596302a7d8e3ee3
Sataponn Phutrakul <nongsam36@hotmail.com>
2019-01-23 20:02:28 +0200

    Get file path

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer')
 var upload = multer({ dest: 'videos' })
+var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,7 @@ router.get('/', function(req, res) {
 router.post('/process', upload.single('video'), function(req, res) {
   console.log(req.file);
   // TODO: Process the video
+  var path = _.get(req, 'file.path');
 
   var output = 'test';
   res.render('output', { output });
