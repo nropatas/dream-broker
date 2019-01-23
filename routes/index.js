70d4fb6268a5aa84108d9514d624640eec6c603a
Sataponn Phutrakul <nongsam36@hotmail.com>
2019-01-23 19:28:44 +0200

    Add route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
 });
+
+router.post('/process', function(req, res) {
+  // TODO: Process the video
+
+  var output = 'test';
+  res.render('output', { output });
+})
 
 module.exports = router;
